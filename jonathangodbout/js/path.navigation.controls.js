9d9ba52059ded11e6f088c101eed9d334b3f6a31
jnthngdbt <jonathangodbout@Jonathan-Godbouts-MacBook-Pro.local>
2017-02-23 23:21:02 -0500

    Added touch events in path navigation controls

--- jonathangodbout/js/path.navigation.controls.js
+++ jonathangodbout/js/path.navigation.controls.js
@@ -10,52 +10,94 @@ function PathNavigationControls( camera, domElement ) {
 
 	this.acceleration = 0.001;
 	this.displacement = 0;
+	this.speed = 0;
+	//this.lookX = 0;
 
     // INTERNAL
 
     this.isDown = false;
 	this.originX = 0;
 	this.originY = 0;
-	this.speed = 0;
-
 
 	this.update = function(){
 		this.displacement += this.speed * this.acceleration;
 	}
 
-	this.mousedown = function( event ) {
-		this.originX = event.screenX;
-		this.originY = event.screenY;
+	this.start = function(x,y){
+		this.originX = x;
+		this.originY = y;
 		this.speed = 0;
+		//this.lookX = 0;
 		this.isDown = true;
-	};
+	}
 
-	this.mousemove = function( event ) {
-		this.speed = this.isDown ? event.screenY-this.originY : 0;;
-	};
+	this.move = function(x,y){
+		if (this.isDown) {
+			this.speed = y-this.originY 
+			//this.lookX += event.screenX-this.originX;
+		} else {
+			this.speed = 0;
+		}		
+	}
 
-	this.mouseup = function( event ) {
+	this.end = function(x,y){
 		this.originX = 0;
 		this.originY = 0;
 		this.speed = 0;
-		this.isDown = false;
+		//this.lookX = 0;
+		this.isDown = false;	
+	}
+
+	this.mousedown = function( event ) {
+		this.start(event.screenX, event.screenY);
 	};
 
+	this.mousemove = function( event ) {
+		this.move(event.screenX, event.screenY);
+	};
+
+	this.mouseup = function( event ) {
+		this.end(event.screenX, event.screenY);
+	};
+
+	function touchstart( event ) {
+		var x = event.touches[ 0 ].pageX;
+		var y = event.touches[ 0 ].pageY;
+		this.start(x,y);
+	}
+
+	function touchmove( event ) {
+		var x = event.touches[ 0 ].pageX;
+		var y = event.touches[ 0 ].pageY;
+		this.move(x,y);
+	}
+
+	function touchend( event ) {
+		var x = event.touches[ 0 ].pageX;
+		var y = event.touches[ 0 ].pageY;
+		this.end(x,y);
+	}
+
 	function bind( scope, fn ) {
-
 		return function () {
-
 			fn.apply( scope, arguments );
-
 		};
-
 	}
 
 	var _mousemove = bind( this, this.mousemove );
 	var _mousedown = bind( this, this.mousedown );
 	var _mouseup = bind( this, this.mouseup );
 
+	var _touchstart = bind( this, this.touchstart );
+	var _touchend = bind( this, this.touchend );
+	var _touchmove = bind( this, this.touchmove );
+
 	this.domElement.addEventListener( 'mousemove', _mousemove, false );
 	this.domElement.addEventListener( 'mousedown', _mousedown, false );
 	this.domElement.addEventListener( 'mouseup',   _mouseup, false );
+
+	this.domElement.addEventListener( 'touchstart', _touchstart, false );
+	this.domElement.addEventListener( 'touchend', _touchend, false );
+	this.domElement.addEventListener( 'touchmove', _touchmove, false );
+
 }
